f760f5df26d8cabc5441ce1b60f0a2642bdde7a8
CA01971092 <107534446+CA01971092@users.noreply.github.com>
2025-04-17 12:23:11 +0900

    Fix: Box component replaced with div to resolve layout error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Pasuteru from "../assets/images/Pasuteru.png";
-import { Box } from "@mui/material";
 import Image from "next/image";
 
 export default function TitlePage() {
     return (
         <>
-            <Box>
-                <Image src={Pasuteru} alt="TitleBackground" layout="fill" />
-            </Box>
+            <div
+                style={{
+                    position: "relative",
+                    width: "100vw",
+                    height: "100vh"
+                }}
+            >
+                <Image src={Pasuteru} alt="TitleBackground" fill />
+            </div>
         </> 
     );
 }
