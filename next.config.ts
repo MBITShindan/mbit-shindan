629adb70a612c236eeb7e5bc094d6e91fa79ad6b
CA01971172 <CA01971172@st.kawahara.ac.jp>
2025-04-30 11:51:13 +0900

    add baseConfig

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,23 @@ import type { NextConfig } from "next";
 // 例えば、`BUILD_TARGET=static`を指定すると、静的エクスポート用の設定が適用されます。
 const isStaticExport: boolean = process.env.BUILD_TARGET === 'static';
 
+//共通の設定
+const baseConfig: NextConfig = {
+    reactStrictMode: true,
+};
+
 const nextConfig: NextConfig = isStaticExport
 ? {
     // SSRを使用しない場合の設定
+    ...baseConfig,
     output: "export",
     images: { unoptimized: true },
     basePath: "/mbit-shindan",
     assetPrefix: "/mbit-shindan/",
 }
 : {
-    reactStrictMode: true,
-    // SSR 環境で使う設定をここに記述
+    // SSRを使用する場合の設定
+    ...baseConfig,
 };
 
 export default nextConfig;
