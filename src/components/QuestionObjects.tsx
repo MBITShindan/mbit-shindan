cb39c898e6441e817aa508f7436c9c97680a64e2
CA01971172 <CA01971172@st.kawahara.ac.jp>
2025-05-15 13:05:15 +0900

    Implement randomPosition

--- src/components/QuestionObjects.tsx
+++ src/components/QuestionObjects.tsx
@@ -4,20 +4,33 @@ import Image from 'next/image'
 import { Grid } from "@mui/material";
 import { questions } from "../mbtiQuestions";
 
+type Position = {
+    x: number;
+    y: number;
+};
+
 export default function QuestionObjects(props: {checkedObjects: string[]}) {
     const { checkedObjects } = props;
-    const [ leftObjects, setLeftObjects ] = useState<string[]>([]);
+    const [ leftObjects, setLeftObjects ] = useState<{[objectName: string]: Position}>({});
 
     useEffect(() => {
         const objects: string[] = Object.keys(questions);
         const filtered = objects.filter(item => !checkedObjects.includes(item));
         const selected = filtered.sort(() => Math.random() - 0.5).slice(0, 8);
-        setLeftObjects(selected);
+        // xとyの値をランダムに-30から30の範囲で生成
+        const positions: {[objectName: string]: Position} = {};
+        selected.forEach((object) => {
+            positions[object] = {
+                x: Math.floor(Math.random() * 60) - 30,
+                y: Math.floor(Math.random() * 60) - 30
+            };
+        });
+        setLeftObjects(positions);
     }, []);
 
     return (
         <Grid container spacing={0} style={{flexGrow: 1}}>
-            {leftObjects.map((object, index) => (
+            {Object.keys(leftObjects).map((object, index) => (
                 <Grid
                     key={index}
                     size={6}
@@ -25,11 +38,15 @@ export default function QuestionObjects(props: {checkedObjects: string[]}) {
                 >
                     <div className="relative w-full h-full">
                         <Image
-                        src={`/objects/${object}.png`}
-                        alt={object}
-                        fill
-                        style={{ objectFit: "contain" }}
-                    />
+                            src={`/objects/${object}.png`}
+                            alt={object}
+                            fill
+                            style={{
+                                objectFit: "contain",
+                                marginLeft: `${leftObjects[object].x}px`,
+                                marginTop: `${leftObjects[object].y}px`
+                            }}
+                        />
                     </div>
                 </Grid>
             ))}
