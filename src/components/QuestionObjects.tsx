65cf8fcd3fe1a97e273018b9eabaa73382e92b8f
CA01971172 <CA01971172@st.kawahara.ac.jp>
2025-05-19 10:15:24 +0900

    Fix the type of Position

--- src/components/QuestionObjects.tsx
+++ src/components/QuestionObjects.tsx
@@ -5,6 +5,7 @@ import { Grid } from "@mui/material";
 import { questions } from "../mbtiQuestions";
 
 type Position = {
+    id: string;
     x: number;
     y: number;
 };
@@ -13,43 +14,43 @@ const animations = ['animate-breathe1', 'animate-breathe2', 'animate-breathe3'];
 
 export default function QuestionObjects(props: {checkedList: string[]}) {
     const { checkedList } = props;
-    const [ leftObjects, setLeftObjects ] = useState<{[objectName: string]: Position}>({});
+    const [ leftObjects, setLeftObjects ] = useState<(Position | null)[]>([]);
 
     useEffect(() => {
         const objects: string[] = Object.keys(questions);
         const filtered = objects.filter(item => !checkedList.includes(item));
         const selected = filtered.sort(() => Math.random() - 0.5).slice(0, 8);
         // xとyの値をランダムに-30から30の範囲で生成
-        const positions: {[objectName: string]: Position} = {};
-        selected.forEach((object) => {
-            positions[object] = {
-                x: Math.floor(Math.random() * 60) - 30,
-                y: Math.floor(Math.random() * 60) - 30
-            };
-        });
+        const positions: Position[] = selected.map((object) => ({
+            id: object,
+            x: Math.floor(Math.random() * 60) - 30,
+            y: Math.floor(Math.random() * 60) - 30
+        }));
         setLeftObjects(positions);
     }, []);
 
     return (
         <Grid container spacing={0} style={{flexGrow: 1}}>
-            {Object.keys(leftObjects).map((object, index) => (
+            {(leftObjects).map((object, index) => (
                 <Grid
                     key={index}
                     size={6}
                     className="p-6"
                 >
                     <div className="relative w-full h-full">
-                        <Image
-                            src={`/objects/${object}.png`}
-                            alt={object}
-                            fill
-                            className={`${animations[Math.floor(Math.random() * animations.length)]}`}
-                            style={{
-                                objectFit: "contain",
-                                marginLeft: `${leftObjects[object].x}px`,
-                                marginTop: `${leftObjects[object].y}px`
-                            }}
-                        />
+                        {object && (
+                            <Image
+                                src={`/objects/${object.id}.png`}
+                                alt={object.id}
+                                fill
+                                className={`${animations[Math.floor(Math.random() * animations.length)]}`}
+                                style={{
+                                    objectFit: "contain",
+                                    marginLeft: `${object.x}px`,
+                                    marginTop: `${object.y}px`
+                                }}
+                            />
+                        )}
                     </div>
                 </Grid>
             ))}
