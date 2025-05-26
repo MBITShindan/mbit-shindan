a3fe76b779d2b1104c3978b2b0817455d4bb992d
CA01971172 <CA01971172@st.kawahara.ac.jp>
2025-05-26 12:27:02 +0900

    Implement RankingBox

--- src/app/rankings/page.tsx
+++ src/app/rankings/page.tsx
@@ -1,32 +1,57 @@
 import { Box } from "@mui/material";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ENDPOINTS } from "../../lib/constants";
+import { cookies } from "next/headers";
+import StackedBarChartIcon from '@mui/icons-material/StackedBarChart';
+import RankingBox from "../../components/RankingBox";
+import { MBTIType } from "../../diagnosisResults";
+
+type RankingData = {
+    type: string;
+    ratio: number;
+}
 
 export default async function RankingPage() {
+    // ランキングデータを取得
     const res = await fetch(`${ENDPOINTS.rankings}`, {
         // cache: 'no-store',
     });
-    const data = await res.json();
+    const data: {data: RankingData[]} = await res.json();
+    const rankings: RankingData[] = data.data;
+
+    // 性格診断結果を取得
+    const cookieStore = await cookies();
+    const personalityResult: string | undefined = cookieStore.get("personalityResult")?.value;
 
     return (
         <AppRouterCacheProvider>
             <Box
-                sx={{
-                    width: "100vw",
-                    height: "100dvh",
-                    overflow: "hidden",
-                    position: "fixed",
-                    backgroundImage: "url('pastel2.png')",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    backgroundRepeat: "no-repeat",
-                    display: "flex",
-                    gap: "1rem",
-                    flexDirection: "column",
-                    justifyContent: "flex-start",
-                    pb: "2rem",
-                }}
+                className="
+                    w-screen h-[100dvh] 
+                    overflow-hidden 
+                    fixed 
+                    bg-[url('/pastel2.png')] bg-cover bg-center bg-no-repeat 
+                    flex flex-col gap-4 items-center justify-start 
+                    text-[#3C4F69]
+                    p-4
+                "
             >
+                <Box>
+                    <span className="text-3xl font-bold flex items-center">
+                        <StackedBarChartIcon sx={{fontSize: "2.5rem"}}/>
+                        診断ランキング
+                    </span>
+                </Box>
+                <Box className="flex flex-col gap-2 w-full flex-grow overflow-y-auto">
+                    {rankings.map((rankingData, index) => (
+                        <RankingBox
+                            key={rankingData.type}
+                            rank={index + 1}
+                            type={rankingData.type as MBTIType}
+                            ratio={rankingData.ratio}
+                        />
+                    ))}
+                </Box>
                 {JSON.stringify(data, null, 2)}
             </Box>
         </AppRouterCacheProvider>
---
+++ src/components/RankingBox.tsx
@@ -0,0 +1,33 @@
+import { Box } from "@mui/material";
+import Image from "next/image";
+import { diagnosisResults, MBTIType } from "../diagnosisResults";
+
+export default async function RankingBox(props: {
+    rank: number,
+    type: MBTIType,
+    ratio: number;
+}) {
+    const { rank, type, ratio } = props;
+
+    return (
+        <Box className="flex items-center justify-center text-3xl font-bold gap-4">
+            <Box>{rank}位</Box>
+            <Box
+                className={`
+                    bg-white
+                    rounded-lg shadow-md max-w-md p-2
+                    flex items-center flex-grow gap-2 justify-between
+                `}
+            >
+                <Image
+                    src={`/mbti/${type}.png`}
+                    alt={type}
+                    width={20}
+                    height={20}
+                />
+                <Box>{diagnosisResults[type].name}</Box>
+                <Box>{ratio}%</Box>
+            </Box>
+        </Box>
+    );
+}
