1d0a03e214a6dd05e3bbe8e83f49f5a25060f575
CA01971172 <CA01971172@st.kawahara.ac.jp>
2025-05-27 09:53:59 +0900

    Fit theme color

--- src/components/RankingBox.tsx
+++ src/components/RankingBox.tsx
@@ -8,10 +8,29 @@ export default async function RankingBox(props: {
 }) {
     const { rank, type, ratio } = props;
 
+    // 診断結果のテーマカラーを取得する関数
+    function getTypeColor(mbtiType: string = type): string{
+        let result = "bg-white";
+        if(type.includes("N")){
+            if(type.includes("T")){ // 分析家タイプ(N+T)
+                result = "bg-[#DFB3EF]"; // 紫色
+            }else{ // 外交官タイプ(N+F)
+                result = "bg-[#D2F1B4]"; // 緑色
+            }
+        }else if(type.includes("S")){
+            if(type.includes("J")){ // 番人タイプ(S+J)
+                result = "bg-[#B4E7F1]"; // 青色
+            }else{ // 探検家タイプ(S+P)
+                result = "bg-[#F1E7B4]"; // 黄色
+            }
+        }
+        return result;
+    }
+
     return (
         <div className="flex items-center gap-5 text-3xl font-bold">
             <div className="w-15 text-nowrap">{rank}位</div>
-            <div className="bg-white rounded-lg shadow-md p-2 flex items-center justify-between w-full gap-2">
+            <div className={getTypeColor() + " rounded-lg shadow-md p-2 flex items-center justify-between w-full gap-2"}>
                 <Image
                     src={`/mbti/${type}.png`}
                     alt={type}
