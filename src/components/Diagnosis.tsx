3266e82ffcfebe1040e4073fb1a7833e57ca9bda
CA01971002 <107534434+CA01971002@users.noreply.github.com>
2025-07-09 11:39:51 +0900

    Added compatibility rankings.

--- src/components/Diagnosis.tsx
+++ src/components/Diagnosis.tsx
@@ -1,168 +1,130 @@
+"use client";
+
 import Image from "next/image";
 import { diagnosisResults, MBTIType } from "../diagnosisResults";
+import { useEffect, useState } from "react";
 import Diversity1Icon from '@mui/icons-material/Diversity1Outlined';
 import PersonPinCircleIcon from '@mui/icons-material/PersonPinCircleOutlined';
 import FlatwareIcon from '@mui/icons-material/FlatwareOutlined';
-import EmojiEventsIcon from '@mui/icons-material/EmojiEventsOutlined';
-import MilitaryTechIcon from '@mui/icons-material/MilitaryTechOutlined';
-import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremiumOutlined';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import DiagnosisAisyou from "./DiagnosisAisyou";
+import { ENDPOINTS } from "../lib/constants";
 
-const matchIcons = [
-  <EmojiEventsIcon key={0}/>,
-  <WorkspacePremiumIcon key={1}/>,
-  <MilitaryTechIcon key={2}/>
-];
-const matchBgColors = ["#FDECC8", "#F9E0DC", "#E2ECF7"];
+const matchBgColors = ["#FDECC8", "#F9E0DC", "#E2ECF7", "#DFE4EA"];
 
 type Props = {
   personality: MBTIType;
 };
 
+//レスポンスデータ
+interface Compatibility {
+  type: string;
+  percentage: number;
+}
+
+interface CompatibilityData {
+  baseType: string;
+  compatibilities: Compatibility[];
+}
+
 export default function Diagnosis({ personality }: Props) {
   const result = diagnosisResults[personality];
+  const [goodTypes, setGoodTypes] = useState<CompatibilityData|null>(null);
 
-  if (!result) {
-    return <p>診断結果が見つかりません。</p>;
-  }
+  useEffect(() => {
+    const fetchGoodTypes = async () => {
+      try {
+        const res = await fetch(`${ENDPOINTS.compatibility}?personalityType=${personality}`);
+        if (!res.ok) {
+          console.error("Fetch failed with status:", res.status)
+          return;
+        }
+      const json:CompatibilityData = await res.json();
+      setGoodTypes(json);
+      console.log(json)
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+    fetchGoodTypes();
+  }, [personality]);
+
+  if (!result) return <p>診断結果が見つかりません。</p>;
 
   return (
-      <div
-        style={{
-          padding: "0.5rem",
-          margin: "0 auto",
-          textAlign: "center",
-          color: "#3C4F69",
-          fontWeight: 600,
-        }}
-      >
-        {/* タイトル */}
-        <h1 style={{ fontSize: "1.2rem", marginBottom: "0.25rem" }}>
-          あなたは{" "}
-          <span style={{ fontSize: "1.9rem" }}>{result.name}</span>{" "}
-          タイプ！
-        </h1>
+    <div style={{ padding: "0.5rem", textAlign: "center", color: "#3C4F69", fontWeight: 600 }}>
+      <h1 style={{ fontSize: "1.2rem", marginBottom: "0.25rem" }}>
+        あなたは <span style={{ fontSize: "1.9rem" }}>{result.name}</span> タイプ！
+      </h1>
 
-        {/* キャラクター画像 */}
-        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", marginTop: "0.5rem", marginLeft: "5rem" }}>
-          <Image
-            src={result.image.replace("../public", "")}
-            alt={`${result.name}の画像`}
-            width={80}
-            height={100}
-            unoptimized
-            style={{
-              marginBottom: "1.5rem",
-            }}
-          />
-          {/* <p style={{ fontSize: "1.125rem", color: "#666", marginTop: "-1rem", marginRight: "5rem" }}> */}
-          <p style={{ fontSize: "1.125rem", color: "#666", marginTop: "-1rem" }}>
-            タイプ<br />【{result.type}】
-          </p>
+      <div style={{ display: "flex", justifyContent: "center", marginTop: "0.5rem" }}>
+        <Image
+          src={result.image.replace("../public", "")}
+          alt={`${result.name}の画像`}
+          width={80}
+          height={100}
+          unoptimized
+          style={{ marginBottom: "1.5rem" }}
+        />
+        <p style={{ fontSize: "1.125rem", color: "#666", marginTop: "-1rem" }}>
+          タイプ<br />【{result.type}】
+        </p>
+      </div>
+
+      <div style={{ textAlign: "left", marginTop: "-1rem", marginBottom: "-0.5rem" }}>
+        <div style={{ fontSize: "1.5rem", display: "flex", alignItems: "center", marginBottom: "0.5rem" }}>
+          <PersonPinCircleIcon style={{ marginRight: "0.5rem" }} />
+          性格説明
         </div>
+        <p style={{ fontSize: "0.875rem" }}>{result.description[0]}</p>
+        <p style={{ fontSize: "0.875rem" }}>{result.description[1]}</p>
+      </div>
 
-        {/* 性格説明 */}
-        <div style={{ textAlign: "left", marginTop: "-1rem", marginBottom: "-0.5rem" }}>
-          <div style={{ fontSize: "1.5rem", display: "flex", alignItems: "center", marginBottom: "0.5rem" }}>
-            <PersonPinCircleIcon style={{ fontSize: "1.8rem", marginRight: "0.5rem" }} />
-            性格説明
-          </div>
-          <p style={{ fontSize: "0.875rem" }}>{result.description[0]}</p>
-          <p style={{ fontSize: "0.875rem" }}>{result.description[1]}</p>
+      <div style={{ textAlign: "left", marginTop: "0.5rem" }}>
+        <div style={{ fontSize: "1.25rem", display: "flex", alignItems: "center" }}>
+          <Diversity1Icon style={{ marginRight: "0.5rem" }} />
+          相性のいいタイプは…
         </div>
-
-        {/* 相性の良いタイプ */}
-        <div style={{ textAlign: "left", marginTop: "0.5rem", marginBottom: "-0.5rem" }}>
-          <div style={{ fontSize: "1.25rem", display: "flex", alignItems: "center" }}>
-            <Diversity1Icon style={{ marginRight: "0.5rem" }} />
-            相性のいいタイプは…
-          </div>
-          <div
-            style={{
-              display: "grid",
-              gridTemplateColumns: "1fr 1fr",
-              gap: "0.5rem",
-              marginTop: "0.5rem",
-            }}
-          >
-            {result.goodMatches.map((match, index) => (
-              <div
-                key={match}
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  padding: "0.5rem 1.25rem",
-                  borderRadius: "0.75rem",
-                  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-                  fontSize: "0.875rem",
-                  backgroundColor: matchBgColors[index] || "#3C4F69",
-                  opacity: 0.8,
-                }}
-              >
-                <span style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
-                  {matchIcons[index]}
-                  {diagnosisResults[match as MBTIType]?.name ?? match}
-                </span>
-                <span style={{ color: "#3C4F69" }}>
-                  {[13, 7, 20][index] || 10}%
-                </span>
-              </div>
-            ))}
-            {/* その他 */}
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                padding: "0.5rem 1.25rem",
-                borderRadius: "0.75rem",
-                boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-                fontSize: "0.875rem",
-                backgroundColor: "#DFE4EA",
-                opacity: 0.8,
-              }}
-            >
-              <span style={{ display: "flex", alignItems: "center", gap: "0.25rem" }}>
-                <MoreHorizIcon />
-                その他
-              </span>
-              <span style={{ color: "#555" }}>
-                {
-                  100 - [13, 7, 20]
-                    .slice(0, result.goodMatches.length)
-                    .reduce((acc, val) => acc + val, 0)
-                }%
-              </span>
-            </div>
-          </div>
-        </div>
-
-        {/* 食べ物のおすすめ */}
-        <div style={{ textAlign: "left", marginTop: "1rem" }}>
-          <div style={{ fontSize: "1.25rem", display: "flex", alignItems: "center", marginBottom: "0.5rem" }}>
-            <FlatwareIcon style={{ marginRight: "0.5rem" }} />
-            おすすめ屋台グルメ！
-          </div>
-          <div style={{ display: "flex", alignItems: "start", gap: "1rem", marginTop: "1rem" }}>
-            <p style={{ fontSize: "0.875rem", marginTop: "-1.25rem" }}>
-              {result.recommendedFoods.description[0]}
-              <br />
-              {result.recommendedFoods.description[1]}
-            </p>
-            <Image
-              src={result.recommendedFoods.image.replace("../public", "")}
-              alt={`${result.recommendedFoods.name}の画像`}
-              width={120}
-              height={120}
-              unoptimized
-              style={{
-                borderRadius: "0.5rem",
-                marginTop: "-0.5rem",
-              }}
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gap: "0.5rem",
+            marginTop: "0.5rem",
+          }}
+        >
+          {goodTypes && goodTypes.compatibilities.map((match, index:number) => (
+            <DiagnosisAisyou
+              key={index}
+              name={match.type}
+              rate={`${match.percentage}`}
+              image={`/mbti/${match.type}.png`}
+              color={matchBgColors[index % matchBgColors.length]}
             />
-          </div>
+          ))}
         </div>
       </div>
+
+      <div style={{ textAlign: "left", marginTop: "1rem" }}>
+        <div style={{ fontSize: "1.25rem", display: "flex", alignItems: "center", marginBottom: "0.5rem" }}>
+          <FlatwareIcon style={{ marginRight: "0.5rem" }} />
+          おすすめ屋台グルメ！
+        </div>
+        <div style={{ display: "flex", gap: "1rem", marginTop: "1rem" }}>
+          <p style={{ fontSize: "0.875rem", marginTop: "-1.25rem" }}>
+            {result.recommendedFoods.description[0]}
+            <br />
+            {result.recommendedFoods.description[1]}
+          </p>
+          <Image
+            src={result.recommendedFoods.image.replace("../public", "")}
+            alt={`${result.recommendedFoods.name}の画像`}
+            width={120}
+            height={120}
+            unoptimized
+            style={{ borderRadius: "0.5rem", marginTop: "-0.5rem" }}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
