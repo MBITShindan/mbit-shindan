b8bc4f9e16b7cdfb5ef590db7228ac0f4b9cc598
CA01971028 <CA01971028@st.kawahara.ac.jp>
2025-07-12 12:17:42 +0900

    文字透明化修正

--- src/components/ModalButton.tsx
+++ src/components/ModalButton.tsx
@@ -97,7 +97,7 @@ export default function ModalButton(props: MainButtonProps) {
                                     mt: "2rem"
                                 }}
                             >
-                                <span className="text-lg text-left">{value.answer}</span>
+                                <span className="text-lg text-left text-black">{value.answer}</span>
                             </Button>
                         </Box>
                     ))}
